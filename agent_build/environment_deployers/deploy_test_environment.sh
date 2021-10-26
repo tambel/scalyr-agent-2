9afa5c33dff89857022f8188c9e13135d0722c66
arthur <arthurk@sentinelone.com>
2021-10-26 20:14:42 +0300

    Test

--- agent_build/environment_deployers/deploy_test_environment.sh
+++ agent_build/environment_deployers/deploy_test_environment.sh
@@ -3,13 +3,14 @@ SOURCE_ROOT=$(dirname "$(dirname "$(dirname "$0")")")
 CACHE_DIR="${1}"
 
 use_cache=false
+save_cache=false
 
 if [ -n "$CACHE_DIR" ]; then
   if [ ! -d "$CACHE_DIR" ]; then
-    >&2 echo "Cache directory '${CACHE_DIR}' does not exist."
-    exit 1
-    use_cache=true
+    mkdir -p "${CACHE_DIR}"
+    save_cache=true
   fi
+  use_cache=true
 fi
 
 if $use_cache ; then
@@ -17,3 +18,7 @@ if $use_cache ; then
 fi
 
 python3 -m pip install -r "${SOURCE_ROOT}/dev-requirements.txt"
+
+if $save_cache ; then
+  cp -a ~/.cache/pip "$CACHE_DIR/pip"
+fi
