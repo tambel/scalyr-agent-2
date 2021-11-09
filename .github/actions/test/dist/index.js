4ed80d1532298e9f22336d52ec6a3222770340e8
arthur <arthurk@sentinelone.com>
2021-11-09 15:04:11 +0300

    Test

--- .github/actions/test/dist/index.js
+++ .github/actions/test/dist/index.js
@@ -8442,16 +8442,27 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2619);
 const github = __nccwpck_require__(4637);
+const fs = __nccwpck_require__(5747);
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const cacheDir = core.getInput('cache-dir');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+
+  filenames = fs.readdirSync(cacheDir);
+
+  console.log("\nCurrent directory filenames:");
+  filenames.forEach(file => {
+    console.log(file);
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
