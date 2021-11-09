734c688dcf93671fa168916cee275cc0a55ed200
arthur <arthurk@sentinelone.com>
2021-11-09 15:34:34 +0300

    Test

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -16,7 +16,6 @@ async function f() {
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 
     if ( fs.existsSync(cacheDir)) {
 
