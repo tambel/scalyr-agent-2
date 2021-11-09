0eb24e4ad574ca78f99776909263df6267969959
arthur <arthurk@sentinelone.com>
2021-11-09 17:08:31 +0300

    Test

--- .github/scripts/get-home-path.py
+++ .github/scripts/get-home-path.py
@@ -2,5 +2,5 @@ import pathlib
 
 cache_path = pathlib.Path.home() / "build-caches"
 
-print(str(cache_path.absolute()).replace("\\, \\\\"))
+print(str(cache_path.absolute()).replace("\\", "\\\\"))
 
