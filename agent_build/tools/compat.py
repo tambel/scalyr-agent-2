13ce6b0b68c23a9ceba57ee9f3e97f18f936bfa5
arthur <arthurk@sentinelone.com>
2021-12-20 18:11:28 +0300

    Fix logger for Python< 3.8

--- agent_build/tools/compat.py
+++ agent_build/tools/compat.py
@@ -15,9 +15,19 @@
 import sys
 import shlex
 
+# Fix some compatibility issues since our Release pipeline relies on a Python < 3.8
 if sys.version_info < (3, 8):
     # Add join function to the shlex module if version of the Python < 3.8
     def shlex_join(args):
         return " ".join(shlex.quote(a) for a in args)
 
     shlex.join = shlex_join
+
+    import logging
+    original_logger_log = logging.Logger._log
+
+    # monkey patch logger's _log method so it ignores 'stacklevel' argument.
+    def logger_log(*args, stacklevel=None, **kwargs):
+        return original_logger_log(*args, **kwargs)
+
+    logging.Logger._log = logger_log
