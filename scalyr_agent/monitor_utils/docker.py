3c18feac09775c6082b7c83ab409aeff208395f0
arthur <arthurk@sentinelone.com>
2022-07-12 18:50:24 +0300

    test

--- scalyr_agent/monitor_utils/docker.py
+++ scalyr_agent/monitor_utils/docker.py
@@ -53,6 +53,6 @@ def get_full_api_socket_path_if_supported(path):
     """
     if platform.system() == "Windows":
 
-        return "npipe:%s" % path
+        return "npipe://%s" % path
     else:
         return "unix:/%s" % path
