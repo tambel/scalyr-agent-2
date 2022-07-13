57aa4dcbba0ffff2ff18ea4daa86c619897ee408
arthur <arthurk@sentinelone.com>
2022-07-13 11:46:09 +0300

    test

--- scalyr_agent/builtin_monitors/kubernetes_monitor.py
+++ scalyr_agent/builtin_monitors/kubernetes_monitor.py
@@ -4489,7 +4489,6 @@ cluster.
                     verify_https=self._global_config.k8s_verify_kubelet_queries,
                 )
         except Exception as e:
-            raise
             self._logger.error(
                 "Error creating KubeletApi object. Kubernetes metrics will not be logged: %s"
                 % six.text_type(e)
